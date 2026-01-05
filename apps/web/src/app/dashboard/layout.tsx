86ebd12165991bfcb6569f26385a3862f9be89f2
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-01-05 17:36:30 +0200

    style: format children prop indentation in DashboardLayout component

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,14 +1,14 @@
 import Header from "@/components/header";
 
 export default function DashboardLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <div className="grid grid-rows-[auto_1fr] h-svh">
-      <Header />
-      {children}
-    </div>
-  );
+    return (
+        <div className="grid grid-rows-[auto_1fr] h-svh">
+            <Header />
+            {children}
+        </div>
+    );
 }
