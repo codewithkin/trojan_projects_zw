6f218848833295250a1787477255938b7bd7354d
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-01-05 16:46:17 +0200

    chore: replace localhost with API_URL env variable using device IP (10.255.235.15)

--- apps/native/lib/auth-client.ts
+++ apps/native/lib/auth-client.ts
@@ -5,7 +5,7 @@ import Constants from "expo-constants";
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-  baseURL: env.EXPO_PUBLIC_SERVER_URL,
+  baseURL: env.EXPO_PUBLIC_API_URL,
   plugins: [
     expoClient({
       scheme: Constants.expoConfig?.scheme as string,
